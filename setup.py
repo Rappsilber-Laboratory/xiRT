99e2cc7895d7eb4a834bc97e3fbcdb9312d09f53
Sven Giese <sven.giese88@gmail.com>
2020-07-07 12:40:45 +0200

    fixed import

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 import sys
 from shutil import rmtree
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = 'xiRT'
