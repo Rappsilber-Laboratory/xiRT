[bug fix] fix class computation from softmax/sigmoid and pep8 stuff

tests/test_predictor.py
@@ -165,6 +165,8 @@ def test_store_predictions():
     xirtnetwork.output_p["scx-activation"] = "softmax"
     xirtnetwork.output_p["hsax-activation"] = "sigmoid"
     # 3 classes fake
+    # scx is 1-hot / softmax encoding -> max = class
+    # hsax is ordinal / sigmoid encoding -> first val <= 0.5 or last entry = class
     predictions = [np.array([[0, 0.1, 0.9], [0.5, 0.4, 0.3], [0.2, 0.8, 0.2]]),
                    [1, 2, 3],
                    np.array([[0.4, 0.24, 0.1], [0.6, 0.24, 0.1], [0.9, 0.6, 0.5]])]
@@ -172,9 +174,9 @@ def test_store_predictions():
 
     training_data.store_predictions(xirtnetwork, predictions, store_idx, cv=cv)
     exp_idx = psms_df.index
-    exp_scx = [3, 1, 2]
+    exp_scx = [2, 0, 1]
     exp_rp = [1, 2, 3]
-    exp_hsax = [1, 2, 3]
+    exp_hsax = [0, 1, 2]
     assert np.all(training_data.prediction_df.loc[exp_idx]["scx-prediction"] == exp_scx)
     assert np.all(training_data.prediction_df.loc[exp_idx]["rp-prediction"] == exp_rp)
     assert np.all(training_data.prediction_df.loc[exp_idx]["hsax-prediction"] == exp_hsax)

xirt/__main__.py
@@ -214,9 +214,9 @@ def xirt_runner(peptides_file, out_dir, xirt_loc, setup_loc, nrows=None, perform
 
     if has_ordinal:
         accs_tmp = xr.compute_accuracy(xirtnetwork.model.predict(xu),
-                                    training_data.psms.loc[training_data.predict_idx],
-                                    xirtnetwork.tasks, xirtnetwork.output_p)
-        eval_unvalidation.extend(np.hstack([-1, accs_tmp, "Unvalidation"]))
+                                       training_data.psms.loc[training_data.predict_idx],
+                                       xirtnetwork.tasks, xirtnetwork.output_p)
+        eval_unvalidation.extend(np.hstack([-1, "Unvalidation", accs_tmp]))
     else:
         eval_unvalidation.extend([-1, "Unvalidation"])
     model_summary_df.loc[len(model_summary_df)] = eval_unvalidation

xirt/predictor.py
@@ -297,24 +297,24 @@ class ModelData:
 
 
 def compute_accuracy(predictions, expected, tasks, params):
-        """
-        Compute accuracy for ordinal predictions.
+    """
+    Compute accuracy for ordinal predictions.
 
-        Args:
-            predictions: ar-like, multi-task predictions from xirt
-            expected: dataframe, pandas dataframe with indices matching the predictions
-            tasks: ar-like, lists of tasks
-            params: dict, parameter
-        Returns:
-            list, accuracy values for tasks with ordinal scale
-        """
-        accuracy_tmp = []
-        # compute accuracy
-        for task_i, pred_ar in zip(tasks, predictions):
-            if "ordinal" in params[task_i + "-column"]:
-                accuracy_tmp.append(accuracy_score(sigmoid_to_class(pred_ar),
-                                                   expected[task_i + "_0based"]))
-        return np.round(accuracy_tmp, 3)
+    Args:
+        predictions: ar-like, multi-task predictions from xirt
+        expected: dataframe, pandas dataframe with indices matching the predictions
+        tasks: ar-like, lists of tasks
+        params: dict, parameter
+    Returns:
+        list, accuracy values for tasks with ordinal scale
+    """
+    accuracy_tmp = []
+    # compute accuracy
+    for task_i, pred_ar in zip(tasks, predictions):
+        if "ordinal" in params[task_i + "-column"]:
+            accuracy_tmp.append(accuracy_score(sigmoid_to_class(pred_ar),
+                                               expected[task_i + "_0based"]))
+    return np.round(accuracy_tmp, 3)
 
 
 def sigmoid_to_class(predictions, t=0.5):