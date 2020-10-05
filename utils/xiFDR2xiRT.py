b2bc630ff989a700ce225ec83a4bc65eb8bf1e3e
Sven Giese <sven.giese88@gmail.com>
2020-10-05 17:01:30 +0200

    simplyfy convesion

--- utils/xiFDR2xiRT.py
+++ utils/xiFDR2xiRT.py
@@ -29,27 +29,6 @@ def rename_columns_crosslinks(psms_df):
     psms_df.rename(columns={"Actual_RT": "rp"}, inplace=True)
     psms_df.rename(columns={"ElutionStart": "rp"}, inplace=True)
     return psms_df
-
-
-def annotate_fraction(name, prefix="SCX"):
-    """
-    Use a regular expression to retrieve the fraction number.
-
-    Parameters:
-    -----------
-    name: str,
-            filename of the raw file (with fraction encoding in the name)
-    prefix: str,
-            prefix for the annotation of the RT
-
-    Note:
-    ----
-    To successfully extract the fractions a defined format in the run name is required.
-    E.g. ]: 'XXXXXXX_SCX20_hSAX06_XXXX' is a valid format.
-    """
-    return int(re.search(r"{}(\d+)".format(prefix), name).groups()[0])
-
-
 description = """
     xiFDR2xiRT - a convenience script to generate a minimal input for using xiRT from xiFDR data.
     
@@ -79,7 +58,8 @@ df_csms = df_csms.loc[:, ~df_csms.columns.duplicated()]
 df_csms = df_csms[sorted(df_csms.columns)]
 
 #  transform run name to fraction
-df_csms["scx"] = df_csms["Run"].apply(annotate_fraction, args=("SCX",))
-df_csms["hsax"] = df_csms["Run"].apply(annotate_fraction, args=("hSAX",))
+frac_annot = lambda name, prefix: int(re.search(r"{}(\d+)".format(prefix), name).groups()[0])
+df_csms["scx"] = df_csms["Run"].apply(frac_annot, args=("SCX",))
+df_csms["hsax"] = df_csms["Run"].apply(frac_annot, args=("hSAX",))
 df_csms.to_csv(args.out_peptides, sep=",")
 print("Writing file: {}".format(args.out_peptides))
