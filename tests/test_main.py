c2e26dca03e3df9e13b8c747fb66399e07400389
Sven <sven.giese88@gmail.com>
2020-10-06 13:35:53 +0200

    fix input column change in sample data and add test for single task ordinal regression

--- tests/test_main.py
+++ tests/test_main.py
@@ -45,6 +45,19 @@ def test_xirt_runner_rp_crosslinks_cv_refit(tmpdir):
     assert True
 
 
+def test_xirt_runner_scx_crosslinks_cv_refit(tmpdir):
+    # test xirt with rp, crosslinks, cv mode and refit the classifier
+    # test xirt with RP, crosslinks in the training mode
+    xirt_loc = os.path.join(fixtures_loc, "xirt_params_scx.yaml")
+    setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv.yaml")
+    peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
+    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
+                         xirt_loc=xirt_loc, setup_loc=setup_loc, nrows=500, perform_qc=False,
+                         write_dummy=False)
+    assert True
+
+
 def test_xirt_runner_3d_crosslinks_cv(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_3RT.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv.yaml")
