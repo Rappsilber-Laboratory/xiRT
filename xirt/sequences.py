22b99cc58cef4fb43255602ff4d37cfaff6dcd5b
Sven <sven.giese88@gmail.com>
2020-10-23 13:34:14 +0200

    add more information for residue encoding

--- xirt/sequences.py
+++ xirt/sequences.py
@@ -179,7 +179,13 @@ def modify_cl_residues(matches_df, seq_in=["Peptide1", "Peptide2"], reduce_cl=Fa
                 # reasign special crosslinker residue
                 residue = "X"
             else:
-                residue = row["Seqar_" + seq_i][row["LinkPos" + str(seq_id + 1)]]
+                try:
+                    residue = row["Seqar_" + seq_i][row["LinkPos" + str(seq_id + 1)]]
+                except IndexError:
+                    print("List index out of range. Check peptide sequence for unwanted characters")
+                    print(row["Seqar_" + seq_i])
+                    print(seq_id)
+                    
             matches_df.at[idx, "Seqar_" + seq_i][row["LinkPos" + str(seq_id + 1)]] = "cl" + residue
 
 
