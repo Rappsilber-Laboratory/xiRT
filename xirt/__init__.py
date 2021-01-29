ef1da48514de169e5fe0075b7f82ed1f67813f85
Sven <sven.giese88@gmail.com>
2021-01-29 20:54:04 +0100

    add versioneer
    Adds versioneer as version tracking software

--- xirt/__init__.py
+++ xirt/__init__.py
@@ -5,3 +5,7 @@ import os
 files = os.listdir(os.path.dirname(__file__))
 files.remove('__init__.py')
 __all__ = [f[:-3] for f in files if f.endswith(".py")]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
