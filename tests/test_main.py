76c29822d4f282f0710acd807f22bd24fa0ed18c
Falk B. Schimweg <git@falk.schimweg.de>
2025-02-25 16:02:09 +0100

    Formatting

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,13 +11,22 @@ def test_xirt_runner_rp_crosslinks_cv(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_rp.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_cv.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -26,13 +35,22 @@ def test_xirt_runner_rp_crosslinks_train(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_rp.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -47,13 +65,22 @@ def test_xirt_runner_rp_crosslinks_cv_refit(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_rp.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -63,13 +90,22 @@ def test_xirt_runner_scx_crosslinks_cv_refit(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_scx.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_cv.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=1000, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=1000,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -77,13 +113,22 @@ def test_xirt_runner_3d_crosslinks_cv(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_3RT.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -91,13 +136,22 @@ def test_xirt_linear_rp(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_rp_linear.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv_linear.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
@@ -105,13 +159,22 @@ def test_xirt_pseudolinear(tmpdir):
     xirt_loc = os.path.join(fixtures_loc, "xirt_params_3RT.yaml")
     setup_loc = os.path.join(fixtures_loc, "learning_params_training_nocv_pseudolinear.yaml")
     peptides_in = os.path.join(fixtures_loc, "DSS_xisearch_fdr_CSM50percent.csv")
+
     with open(xirt_loc, "r") as file:
-        xirt_params=yaml.safe_load(file)
+        xirt_params = yaml.safe_load(file)
+
     with open(setup_loc, "r") as file:
-        learning_params=yaml.safe_load(file)
-    __main__.xirt_runner(peptides_file=peptides_in, out_dir=tmpdir.mkdir("xiRT_results"),
-                         xirt_params=xirt_params, learning_params=learning_params, nrows=500, perform_qc=False,
-                         write_dummy=False)
+        learning_params = yaml.safe_load(file)
+
+    __main__.xirt_runner(
+        peptides_file=peptides_in,
+        out_dir=tmpdir.mkdir("xiRT_results"),
+        xirt_params=xirt_params,
+        learning_params=learning_params,
+        nrows=500,
+        perform_qc=False,
+        write_dummy=False
+    )
     assert True
 
 
